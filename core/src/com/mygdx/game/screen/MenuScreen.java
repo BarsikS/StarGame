0db3e003950ca20ef027e2d59774f28df8dbf6ba
pndimperial <poteryalparol@mail.ru>
2018-06-29 18:54:15 +0500

    New

--- core/src/com/mygdx/game/screen/MenuScreen.java
+++ core/src/com/mygdx/game/screen/MenuScreen.java
@@ -10,18 +10,25 @@ import com.mygdx.game.base.Base2DClass;
 
 public class MenuScreen extends Base2DClass {
     SpriteBatch batch;
+    Texture img;
+    Vector2 touch, pos, v, direct;
+
 
     public MenuScreen(Game game) {
         super(game);
     }
-    //Texture img;
-    //Vector2 pos, v;
+
 
 
     @Override
     public void show() {
         super.show();
+        img = new Texture("circle.png");
         batch = new SpriteBatch();
+        pos = new Vector2();
+        touch = new Vector2(0f,0f);
+        v = new Vector2(0f,0f);
+        direct = new Vector2(0f, 0f);
     }
 
     @Override
@@ -29,13 +36,32 @@ public class MenuScreen extends Base2DClass {
         super.render(delta);
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        direct.set(touch);
+        if (direct.sub(pos).len() > 0.9f) {
+
+            pos.add(v);
+
+        } else {
+            pos.set(touch);
+        }
         batch.begin();
+        batch.draw(img, pos.x, pos.y);
         batch.end();
+
+
     }
 
     @Override
     public void dispose() {
         batch.dispose();
+        img.dispose();
         super.dispose();
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        touch.set(screenX, Gdx.graphics.getHeight() - screenY);
+        v.set(touch.cpy().sub(pos).setLength(2f));
+        return false;
     }
 }
